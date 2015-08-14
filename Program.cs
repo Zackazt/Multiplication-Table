835f90cdce093cc6b429f30387f2ef2902b4bdd0
Zack Wilson <zackazt44@gmail.com>
2015-08-14 17:55:57 -0400

    Finalizing The Code
    No more class variables, switched to just using the user's input as an
    argument for the "printTable" function.  Various clean-ups.

--- Program.cs
+++ Program.cs
@@ -8,16 +8,13 @@ namespace ConsoleApplication3
 {
     class Program
     {
-        private static int topNumber;
-
         static void Main(string[] args)
         {
             Console.WriteLine("How many numbers?");
-            topNumber = Int32.Parse(Console.ReadLine());  //Changed variable names to fit typical C# style
-            printTable();
+            printTable(Int32.Parse(Console.ReadLine()));              //Removed topNumber as a class variable and used it as a parameter instead.
         }
 
-        private static void printTable()
+        private static void printTable(int topNumber)                 //Changed variable names to fit typical C# style.
         {
             int x = 1;
             for (int i = 1; i <= topNumber; i++)
@@ -25,10 +22,10 @@ namespace ConsoleApplication3
                 for (int z = 1; z <= topNumber; z++)
                 {                 
                     int d = (int)Math.Floor(Math.Log10(z * x) + 1);  //Removed the list of if statements and replaced it with 
-                    Console.Write((z * x) + new String(' ', 7 - d)); // computation. 
+                    Console.Write((z * x) + new String(' ', 7 - d)); //computation. 
                 }
                 x++;
-                Console.WriteLine("\n\n");   //Replaced the for loop for adding new lines.  
+                Console.WriteLine("\n\n");                           //Replaced the for loop for adding new lines.  
             }
             Console.ReadLine();
         }
