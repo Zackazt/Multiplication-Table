e6763153b59cb700755f0e07dad220242e6c7f27
Zack Wilson <zackazt44@gmail.com>
2015-08-14 17:58:55 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication3
@@ -11,12 +7,7 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             Console.WriteLine("How many numbers?");
-<<<<<<< HEAD
             printTable(Int32.Parse(Console.ReadLine()));              //Removed topNumber as a class variable and used it as a parameter instead.
-=======
-            topNumber = Int32.Parse(Console.ReadLine());             //Changed variable names to fit typical C# style
-            printTable();
->>>>>>> origin/master
         }
 
         private static void printTable(int topNumber)                 //Changed variable names to fit typical C# style.
