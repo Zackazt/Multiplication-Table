1d13f374c837b387cdb24a4b0b824c0b23c6d60b
Zack Wilson <zackazt44@gmail.com>
2015-08-21 23:34:10 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,22 +10,22 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many numbers?");
+            Console.WriteLine("How many numbers?");                    //Asks the user how many numbers they would like.
             printTable(Int32.Parse(Console.ReadLine()));               //Removed topNumber as a class variable and used it as a parameter instead.
-            Console.ReadLine();
+            Console.ReadLine();                                        //Pauses the program in order to view.
         }
 
-        private static void printTable(int topNumber)                 //Changed variable names to fit typical C# style.
+        private static void printTable(int topNumber)                  //Changed variable names to fit typical C# style.
         {
-            for (int x = 1; x <= topNumber; x++)
+            for (int x = 1; x <= topNumber; x++)                       //Iterates 1 through the number received.
             {
-                for (int y = 1; y <= topNumber; y++)
+                for (int y = 1; y <= topNumber; y++)                   //Loopception.
                 {
-                    int z = x * y;
-                    int d = (int)Math.Floor(Math.Log10(z) + 1);       //Removed the list of if statements and replaced it with 
-                    Console.Write((z) + new String(' ', 7 - d));      //computation. 
+                    int z = x * y;                                     //Simply combining variables.
+                    int d = (int)Math.Floor(Math.Log10(z) + 1);        //Removed the list of if statements and replaced it with 
+                    Console.Write((z) + new String(' ', 7 - d));       //computation. 
                 }
-                Console.WriteLine("\n\n");                            //Replaced the for loop for adding new lines.  
+                Console.WriteLine("\n\n");                             //Replaced the for loop for adding new lines.  
             }
         }
     }
